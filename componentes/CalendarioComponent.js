8cf54f800c3e5d0a87e0494bbc62485d48ae35a4
mariasanguesa <msanguesa3@gmail.com>
2023-04-13 16:59:25 +0200

    Componentes funcionales de React Nave  #4

--- componentes/CalendarioComponent.js
+++ componentes/CalendarioComponent.js
@@ -2,25 +2,25 @@ import React from 'react';
 import { ListItem, Avatar } from '@rneui/themed';
 import { SafeAreaView, FlatList } from 'react-native';
 function Calendario(props) {
-    const renderCalendarioItem = ({ item, index }) => {
-        return (
-            <ListItem key={index} onPress={() => props.onPress(item.id)} bottomDivider>
-                <Avatar source={require('./imagenes/40Años.png')} />
-                <ListItem.Content>
-                    <ListItem.Title>{item.nombre}</ListItem.Title>
-                    <ListItem.Subtitle>{item.descripcion}</ListItem.Subtitle>
-                </ListItem.Content>
-            </ListItem>
-        );
-    };
-    return (
-        <SafeAreaView>
-            <FlatList
-                data={props.excursiones}
-                renderItem={renderCalendarioItem}
-                keyExtractor={item => item.id.toString()}
-            />
-        </SafeAreaView>
-    );
+ const renderCalendarioItem = ({item, index}) => {
+ return (
+ <ListItem key={index} bottomDivider>
+ <Avatar source={require('./imagenes/40Años.png')} />
+ <ListItem.Content>
+ <ListItem.Title>{item.nombre}</ListItem.Title>
+ <ListItem.Subtitle>{item.descripcion}</ListItem.Subtitle>
+ </ListItem.Content>
+ </ListItem>
+ );
+ };
+ return (
+ <SafeAreaView>
+ <FlatList
+ data={props.excursiones}
+ renderItem={renderCalendarioItem}
+ keyExtractor={item => item.id.toString()}
+ />
+ </SafeAreaView>
+ );
 }
 export default Calendario;
