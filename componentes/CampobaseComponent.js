0640dba7a18220fb3c79b6c7d7602f4130d8f706
mariasanguesa <msanguesa3@gmail.com>
2023-04-13 16:19:01 +0200

    Componentes React Native conflicts #3

--- componentes/CampobaseComponent.js
+++ componentes/CampobaseComponent.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
 import Calendario from './CalendarioComponent';
 import { EXCURSIONES } from '../comun/excursiones';
-
-class Campobase extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      excursiones: EXCURSIONES
-    };
-  }
-
-  render() {
- 
-    return (
-        <Calendario excursiones={this.state.excursiones} />
-    );
-  }
+class CampoBase extends Component {
+ constructor(props) {
+ super(props);
+ this.state = {
+ excursiones: EXCURSIONES
+ };
+ }
+ render() {
+ return (
+ <Calendario excursiones={this.state.excursiones} />
+ );
+ }
 }
-
-export default Campobase;
+export default CampoBase;
