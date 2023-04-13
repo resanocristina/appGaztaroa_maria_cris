5d811d9961bf4ee6bcc2d2e98de4cb045af71994
mariasanguesa <msanguesa3@gmail.com>
2023-04-13 16:14:43 +0200

    Componentes React Native #3

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
+import Campobase from './componentes/CampobaseComponent';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Primera modificación</Text>
+  return(
+  <SafeAreaProvider>
+    <View>
+      <Campobase/>
       <StatusBar style="auto" />
     </View>
+  </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'purple',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
