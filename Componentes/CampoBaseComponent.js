093f6b4dface0006aff5a128204ffdefdc61d3e2
mariasanguesa <msanguesa3@gmail.com>
2023-04-15 13:45:31 +0200

    Drawer Navigation #6

--- Componentes/CampoBaseComponent.js
+++ Componentes/CampoBaseComponent.js
@@ -5,8 +5,11 @@ import DetalleExcursion from './DetalleExcursionComponent';
 import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './HomeComponent';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 function CalendarioNavegador() {
   return (
@@ -37,15 +40,55 @@ function CalendarioNavegador() {
   );
 }
 
+function HomeNavegador() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerMode: 'screen',
+        headerTintColor: '#fff',
+        headerStyle: { backgroundColor: '#015afc' },
+        headerTitleStyle: { color: '#fff' },
+      }}
+    >
+      <Stack.Screen
+        name="Etxea"
+        component={Home}
+        options={{
+          title: 'Campo Base',
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function DrawerNavegador() {
+  return (
+    <Drawer.Navigator
+      initialRouteName=" Drawer"
+      screenOptions={{
+        headerShown: false,
+        drawerStyle: {
+          backgroundColor: '#c2d3da',
+        },
+      }}
+    >
+      <Drawer.Screen name="Home" component={HomeNavegador} />
+      <Drawer.Screen name="Calendario" component={CalendarioNavegador} />
+    </Drawer.Navigator>
+  );
+}
+
+
 class Campobase extends Component {
   render() {
-     return (
+    return (
       <NavigationContainer>
-        <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight }}>
-          <CalendarioNavegador />
+        <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight }}>
+          <DrawerNavegador />
         </View>
-      </NavigationContainer>      
-  );
+      </NavigationContainer>
+    );
   }
 }
 
